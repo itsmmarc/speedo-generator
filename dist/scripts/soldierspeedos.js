a169dce15907a8e820eb7ae64a43c93e585acfaf
itsmmarc <officialmarcusmifsud@gmail.com>
2026-01-03 16:39:33 +1100

    restructure project

--- dist/scripts/soldierspeedos.js
+++ dist/scripts/soldierspeedos.js
@@ -1,8 +1,9 @@
-var digits = document.getElementsByClassName('digits'); // collection of digit elements
-var digitsArray = Array.prototype.slice.call(digits);
-var speedovalue = 0;
-var framerate = 30;
-var frametime = 1000 / framerate;
+"use strict";
+const digits = document.getElementsByClassName('digits'); // collection of digit elements
+const digitsArray = Array.prototype.slice.call(digits);
+let speedovalue = 0;
+const framerate = 30;
+const frametime = 1000 / framerate;
 var SpeedometerType;
 (function (SpeedometerType) {
     SpeedometerType[SpeedometerType["HORIZONTAL"] = 1] = "HORIZONTAL";
@@ -12,14 +13,14 @@ var SpeedometerType;
 })(SpeedometerType || (SpeedometerType = {}));
 // speedo digit counter
 setInterval(function () {
-    digitsArray.forEach(function (item) {
+    digitsArray.forEach(item => {
         item.textContent = speedovalue.toString();
         item.style.setProperty('color', getColor(item));
     });
-    var sine_max = 3500;
-    var sine_min = 0;
-    var sine_period = 6;
-    var sine_rate = (sine_max - sine_min) / sine_period / frametime;
+    let sine_max = 3500;
+    let sine_min = 0;
+    let sine_period = 6;
+    let sine_rate = (sine_max - sine_min) / sine_period / frametime;
     speedovalue += Math.round(sine_rate);
     if (speedovalue > sine_max) {
         speedovalue = sine_min;
@@ -27,7 +28,7 @@ setInterval(function () {
 }, frametime);
 // get speedo color
 function getColor(item) {
-    var speed;
+    let speed;
     if (item.textContent) {
         speed = parseInt(item.textContent);
     }
@@ -49,8 +50,8 @@ function getColor(item) {
 }
 // refactor later to deal with other speedo types
 function isClose(speed) {
-    var HSPEEDO_CLOSE_MIN = 850;
-    var HSPEEDO_CLOSE_MAX = 1350;
+    const HSPEEDO_CLOSE_MIN = 850;
+    const HSPEEDO_CLOSE_MAX = 1350;
     if (speed > HSPEEDO_CLOSE_MIN && speed < HSPEEDO_CLOSE_MAX) {
         return true;
     }
@@ -59,8 +60,8 @@ function isClose(speed) {
     }
 }
 function isGood(speed) {
-    var HSPEEDO_GOOD_MIN = 1050;
-    var HSPEEDO_GOOD_MAX = 1150;
+    const HSPEEDO_GOOD_MIN = 1050;
+    const HSPEEDO_GOOD_MAX = 1150;
     if (speed > HSPEEDO_GOOD_MIN && speed < HSPEEDO_GOOD_MAX) {
         return true;
     }
@@ -69,19 +70,19 @@ function isGood(speed) {
     }
 }
 // SHADOWS
-var enableShadowsBtn = document.getElementById('shadows_enable');
-var disableShadowsBtn = document.getElementById('shadows_disable');
-var shadows = document.getElementsByClassName('digits shadow');
-var shadowsArray = Array.prototype.slice.call(shadows);
-enableShadowsBtn.addEventListener('click', function () {
-    shadowsArray.forEach(function (item) {
+const enableShadowsBtn = document.getElementById('shadows_enable');
+const disableShadowsBtn = document.getElementById('shadows_disable');
+const shadows = document.getElementsByClassName('digits shadow');
+const shadowsArray = Array.prototype.slice.call(shadows);
+enableShadowsBtn.addEventListener('click', () => {
+    shadowsArray.forEach(item => {
         if (item.classList.contains('hidden')) {
             item.classList.remove('hidden');
         }
     });
 });
-disableShadowsBtn.addEventListener('click', function () {
-    shadowsArray.forEach(function (item) {
+disableShadowsBtn.addEventListener('click', () => {
+    shadowsArray.forEach(item => {
         if (!item.classList.contains('hidden')) {
             item.classList.add('hidden');
         }
